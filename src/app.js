b5a42a9a507ee2d75817581cd8c264bf8e50e025
matheusflausino <matheus.negocio@gmail.com>
2018-06-08 17:32:41 -0300

    Rotas e checagem de autenticidade na sessão

--- src/app.js
+++ src/app.js
@@ -1,8 +1,35 @@
 const express = require('express');
+const session = require('express-session');
 const index = require('./routes/index');
 const api = require('./routes/api');
 
 const app = express();
+
+function checkAuth (req, res, next) {
+    console.log('checkAuth ' + req.url + " " + req.session.authenticated);
+    
+    if(!req.url.match(/api/)){
+        if ((!req.session || !req.session.authenticated) && req.url != '/login'){
+            res.redirect('/login');
+            return;
+        }
+
+        if (req.session && req.session.authenticated && req.url == '/login'){
+            res.redirect('/');
+            return;
+        }
+    }
+	next();
+}
+
+app.set('view engine', 'ejs');
+app.use(session({
+    secret : 'certificados',
+    resave: true,
+    saveUninitialized: false
+    }));
+app.use(checkAuth);
+
 //Rotas
 app.use('/', index);
 app.use('/api', api);
