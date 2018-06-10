4ae93f0651171fabd7c995a533af6fdee4a7d861
matheusflausino <matheus.negocio@gmail.com>
2018-06-10 17:39:30 -0300

    Modificações do body parse para todo o projeto

--- src/app.js
+++ src/app.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const session = require('express-session');
+const bodyParser = require('body-parser');
 const index = require('./routes/index');
 const api = require('./routes/api');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 function checkAuth (req, res, next) {
     console.log('checkAuth ' + req.url + " " + req.session.authenticated);
-    
+
     if(!req.url.match(/api/)){
+        if(req.session.admin == true && req.url == '/login'){
+            res.redirect('/painel-adm');
+            return;
+        }
+
         if ((!req.session || !req.session.authenticated) && req.url != '/login'){
             res.redirect('/login');
             return;
